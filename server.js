d6bb9bedfadc797f3672658dd530970feddb2dd2
Heather B <vbchomp@gmail.com>
2021-08-14 18:48:00 -0700

    most of lab 11 done. still need about hald of the second card.

--- server.js
+++ server.js
@@ -2,23 +2,45 @@
 
 require('dotenv').config();
 const express = require('express');
+const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const jwksClient = require('jwks-rsa');
 
-const app = express();
+// This is from jsonwebtoken dock 
+// https://www.npmjs.com/package/jsonwebtoken
+const client = jwksClient({
+  // this url comes from your app on the auth0 dashboard 
+  jwksUri: 'https://dev-4rwdhvtd.us.auth0.com/.well-known/jwks.json'
+});
+
+
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
+// this comes from this jsonwebtoken docs
+function getKey(header, callback){
+  client.getSigningKey(header.kid, function(err, key) {
+    var signingKey = key.publicKey || key.rsaPublicKey;
+    callback(null, signingKey);
+  });
+}
 
+app.get('/test', (request, response) => {
   // TODO: 
   // STEP 1: get the jwt from the headers
+  const token = request.headers.authorization.split(' ')[1];
+  
   // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
-  // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
-  // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
-
+  jwt.verify(token, getKey, {}, function(err, user) {
+    if (err){
+      response.send('invalid token');
+    }
+    // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
+    response.send(user);
+  });
 })
+  // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
